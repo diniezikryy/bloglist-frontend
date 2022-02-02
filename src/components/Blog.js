b067361bc3bb8865f58b2632d80afb2cb1cd7378
Dinie Zikry <77114684+diniezikryy@users.noreply.github.com>
2022-02-02 22:02:18 +0800

    completed exercises 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} | {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const buttonLabel = visible ? "hide" : "show";
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <p>
+          <b>{blog.title}</b> by {blog.author}{" "}
+          <button onClick={toggleVisibility}>{buttonLabel}</button>
+        </p>
+        <div style={showWhenVisible}>
+          <p>Url: {blog.url}</p>
+          <p>
+            Likes: {blog.likes} <button id="like-button">like</button>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
